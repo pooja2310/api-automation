d802634e2f39b49810789fdbabd49e24b86a2223
pooja2310 <53404791+pooja2310@users.noreply.github.com>
2023-08-13 21:17:08 +0530

    Add files

---
+++ APITesting/src/main/java/API/Main.java
@@ -0,0 +1,34 @@
+package API;
+import files.Payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        RestAssured.baseURI= "https://reqres.in";
+       String response =  given().log().all().queryParam("key","pass123").header("Content-Type", "application/json")
+                .body(Payload.addPlace()).when().post("api/users").
+                then().log().all().assertThat().statusCode(201).body("id",equalTo("27"))
+                .header("Server","cloudflare").extract().response().asString();
+       System.out.println("Response : " + response);
+
+       JsonPath js= new JsonPath(response); // jsonpath takes string and convert it to json .also help to parse json.
+       String Job= js.getString("job");
+
+        System.out.println("Job Name : " + Job);
+        given().log().all().queryParam("key","pass123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\",\n" +
+                        "    \"updatedAt\": \"2023-08-13T11:18:19.314Z\"\n" +
+                        "}").when().put("api/users/2")
+                .then().log().all().assertThat().statusCode(200);
+
+    }
+
+}
---
+++ APITesting/src/main/java/files/Payload.java
@@ -0,0 +1,14 @@
+package files;
+
+public class Payload
+{
+    public static String addPlace()
+    {
+         return "{\n" +
+                 "    \"name\": \"morpheus\",\n" +
+                 "    \"job\": \"leader\",\n" +
+                 "    \"id\": \"27\",\n" +
+                 "    \"createdAt\": \"2023-08-13T09:51:34.526Z\"\n" +
+                 "}";
+    }
+}
