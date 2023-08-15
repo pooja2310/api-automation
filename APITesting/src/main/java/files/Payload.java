a3ce6f59acce276efb4d45bef41b73ca9f9c03c9
pooja2310 <kanavajepooja@gmail.com>
2023-08-15 23:45:33 +0530

    New changes added

--- APITesting/src/main/java/files/Payload.java
+++ APITesting/src/main/java/files/Payload.java
@@ -11,4 +11,31 @@ public class Payload
                  "    \"createdAt\": \"2023-08-13T09:51:34.526Z\"\n" +
                  "}";
     }
+    
+    public static String coursePrice()
+    {
+        return "{\n" +
+                "  \"dashboard\": {\n" +
+                "    \"purchaseAmount\": 910,\n" +
+                "    \"website\": \"rahulshettyacademy.com\"\n" +
+                "  },\n" +
+                "  \"courses\": [\n" +
+                "    {\n" +
+                "      \"title\": \"Selenium Python\",\n" +
+                "      \"price\": 50,\n" +
+                "      \"copies\": 6\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"title\": \"Cypress\",\n" +
+                "      \"price\": 40,\n" +
+                "      \"copies\": 4\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"title\": \"RPA\",\n" +
+                "      \"price\": 45,\n" +
+                "      \"copies\": 10\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+    }
 }
