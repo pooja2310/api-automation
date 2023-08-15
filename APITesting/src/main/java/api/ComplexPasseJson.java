6113baeb899c9ec71c739b237f1ac14e8d88d7f3
pooja2310 <kanavajepooja@gmail.com>
2023-08-15 23:47:56 +0530

    Nested json code validation added

---
+++ APITesting/src/main/java/api/ComplexPasseJson.java
@@ -0,0 +1,46 @@
+package api;
+
+import files.Payload;
+import io.restassured.path.json.JsonPath;
+
+public class ComplexPasseJson
+{
+	public static void main(String args[])
+	{
+		JsonPath js = new JsonPath(Payload.coursePrice());
+		
+		// Print number of courses in API
+		int count = js.getInt("courses.size()");
+		System.out.println(count);
+		
+		//Print total purchase amount
+		int totalAmount =js.getInt("dashboard.purchaseAmount");
+		System.out.println(totalAmount);
+		
+		//Print title of first course
+	   String title=js.getString("courses[0].title");
+	   System.out.println(title);
+	   
+	   //Print all title and price of courses
+		for(int i=0; i<count;i++)
+		{
+			System.out.println(js.get("courses["+i+"].title").toString());
+			System.out.println(js.get("courses["+i+"].price").toString());
+		}
+		
+		//Print number of copies of Selenium Python
+		
+		for(int i=0; i<count;i++)
+		{
+			String Coursetitle =js.get("courses["+i+"].title");
+			if(Coursetitle.equalsIgnoreCase("Selenium Python"))
+			{
+			   int NoOfCopies=  js.get("courses["+i+"].copies");
+				System.out.println("Copies : " + NoOfCopies);
+				break;
+			}
+		}
+		
+		
+	}
+}
